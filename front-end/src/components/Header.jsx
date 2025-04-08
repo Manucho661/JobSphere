6f566a50eab34b1a7a1624f31157debef1218e5a
manucho661 <emmanuelwanyonyi49@gmail.com>
2025-04-08 16:46:31 +0300

    Recent

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -9,11 +9,9 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-white">
             <a class="navbar-brand logo" href="#"> <img src="images/logo.png" alt="JobSphere Logo" className="logo-img" />
             JobSphere</a>
-
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -26,23 +26,8 @@ const Home = () => {
     <div className="homeStyle">
       
       <h1 className="text-primary" >JOBSPHERE</h1>
-      <ul>
-        {jobs && jobs.length > 0 ? (
-          jobs.map((job) => (
-            <li key={job.id}>
-              <h3>{job.title}</h3>
-              <p>{job.description}</p>
-              <p><strong>Location:</strong> {job.location}</p>
-              <p><strong>Salary:</strong> ${job.salary}</p>
-            
-            </li>
 
-          ))
-        ) : (
-          <p>No jobs available</p>
-
-        )}
-      </ul>
+      <h1>"Your Future Starts Here — Find the Job You Deserve."</h1>
     </div>
   );
 };
