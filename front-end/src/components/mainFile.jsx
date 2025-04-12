1575e0c05e65e9071f241730ef857fd0bdbb6497
Manucho661 <emmanuelwanyonyi49@gmail.com>
2025-04-12 18:41:02 +0300

    Recent

--- front-end/src/components/mainFile.jsx
+++ front-end/src/components/mainFile.jsx
@@ -28,15 +28,124 @@ const Home = () => {
     <div class="container-fluid job-listing">
       <div class="row">
 
-      <h1 class="text-white">Your Future Starts Here — Find the Job You Deserve.</h1>
+      <h1 class=" landing-header mt-5">Your Future Starts Here — Find the Job You Deserve.</h1>
+              
+      </div>
+{/* start row */}
+      <div class="row ">
 
+        <div class="col-md-8">
+
+        </div>
+
+        <div class="col-md-4  d-flex justify-content-between">
+
+  {/* Start Card */}
+          <div class="card">
+            <div class="card-header search-header-label">
+            Search For Your Dream Job
+            </div>
+            <div class="card-body">
+              <form action="">
+                {/* row */}
+                <div class="row mb-2">
+                  <div class="col-md-12">
+                    <label htmlFor="">Job</label>
+                    <input type="text" class="search-input job" placeholder="Search Tenant..."/>
+                  
+                  </div>
+                </div>
+                {/* End row */}
+
+                {/* row */}
+                <div class="row mb-2">
+                  <div class="col-md-6">
+                    <label htmlFor="">Job Category</label>
+                    <select id="categoryFilter" class="categoryFilter">
+                      <option value="">-- Select Building--</option>
+                      <option value="technology">All</option>
+                      <option value="health">Manucho</option>
+                      <option value="business">Ebenezer</option>
+                      <option value="education">Crown Z</option>
+                    </select>
+                  </div>
+
+                  <div class="col-md-6">
+                    <label htmlFor="">Job Type</label>
+                    <select id="categoryFilter" class="categoryFilter">
+                      <option value="">-- Select Building--</option>
+                      <option value="technology">All</option>
+                      <option value="health">Manucho</option>
+                      <option value="business">Ebenezer</option>
+                      <option value="education">Crown Z</option>
+                    </select>
+                  </div>
+                </div>
+               {/* End row */}
+
+                {/* row */}
+                <div class="row mb-2 ">
+                  <div class="col-md-6">
+                    <label htmlFor="">Job Level</label>
+                    <select id="categoryFilter" class="categoryFilter">
+                      <option value="">-- Select Building--</option>
+                      <option value="technology">All</option>
+                      <option value="health">Manucho</option>
+                      <option value="business">Ebenezer</option>
+                      <option value="education">Crown Z</option>
+                    </select>
+                  </div>
+
+                  <div class="col-md-6">
+                    <label htmlFor="">Experience</label>
+                    <select id="categoryFilter" class="categoryFilter">
+                      <option value="">-- Select Building--</option>
+                      <option value="technology">All</option>
+                      <option value="health">Manucho</option>
+                      <option value="business">Ebenezer</option>
+                      <option value="education">Crown Z</option>
+                    </select>
+                  </div>
+                </div>
+                 {/* End row */}
+
+                  {/* row */}
+                <div class="row mb-2">
+                  <div class="col-md-12">
+                    <label htmlFor="">Expected Salary</label>
+                    <select id="categoryFilter" class="categoryFilter">
+                                <option value="">-- Select Building--</option>
+                                <option value="technology">All</option>
+                                <option value="health">Manucho</option>
+                                <option value="business">Ebenezer</option>
+                                <option value="education">Crown Z</option>
+                    </select>
+
+                  </div>
+                </div>
+                {/* End row */}
+
+                  {/* row */}
+                  <div class="row">
+                    <div class="col-md-12">
+                    <button class="btn search">Search</button>
+                    </div>
+                 
+                  </div>
+                  {/* End row */}
+                </form>
+                
+                              
 
               
+         
+                 </div>
+            
 
-      </div>
+          </div>
+         </div>
 
-      
-     
+      </div>    
       
     </div>
   );
