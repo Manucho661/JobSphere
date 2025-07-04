0cb810fe9e1e662ba70c9e64be491409da82fb39
manucho661 <emmanuelwanyonyi49@gmail.com>
2025-07-04 17:42:48 +0300

    create a job search bar

--- front-end/src/pages/common/HomePage.jsx
+++ front-end/src/pages/common/HomePage.jsx
@@ -1,9 +1,25 @@
 import Header from "../../components/common/Header";
 import { useQuery } from "@tanstack/react-query";
+import "../../styles/Home.css";
 
 const HomePage = ()=>{
     return(
-        <h1 className="text-primary" >JOBSPHERE</h1>
+        <div className="main">
+            {/* search bar */}
+            <div className="container-fluid py-3">
+                <div className="container">
+                    <form className="d-flex justify-content-center" role="search" action="">
+                        <input 
+                            className="form-control me-2 w-50" 
+                            type="search" placeholder="Search Jobs" 
+                            aria-label="Search" 
+                            style={{borderRadius: '8px'}}
+                        />
+                        <button class="btn" type="submit" style={{ backgroundColor: '#FFC107', color: '#00192D', fontWeight: 600 }}>Search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 };
 
