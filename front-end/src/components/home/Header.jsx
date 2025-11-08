51a7a972a97ac643861e4077e013cdc7661cec62
manucho661 <emmanuelwanyonyi49@gmail.com>
2025-11-08 17:49:40 +0300

    recent

--- front-end/src/components/home/Header.jsx
+++ front-end/src/components/home/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import ResponsiveNav from '../responsiveNav';
+import ResponsiveNav from '../ResponsiveNav';
 
 
 const Header = () => {
